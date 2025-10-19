1e2b10840b3eb017cfa3d914a27310fcbb43674f
lucialcc27 <lucialcc27@gmail.com>
2025-10-18 20:16:03 -0500

    publish

--- src/formulas.py
+++ src/formulas.py
@@ -15,12 +15,19 @@ def black_scholes_model(x, t, c, r, v, tt):
     w = x * ss.norm.cdf(d_1) - c * np.exp(-r*(tt-t)) * ss.norm.cdf(d_2)
     return w
 
-
-
 def historical_volatility(history_data,n):
     returns = history_data.pct_change()
     hist_vol = returns.rolling(window=n).std()*np.sqrt(252)
     return hist_vol
+
+def bootstrap_volatility(data, size, rep): 
+    returns = data.pct_change()
+    sample_return_vol = []
+    for i in range(rep): 
+        sample = returns.sample(size, replace = True)
+        sample_vol = sample.std()*np.sqrt(252)
+        sample_return_vol.append(sample_vol)
+    return np.array(sample_return_vol)
 
 def vega(x, t, c, r, v, tt):
      d_1 = (np.log(x/c) + (r + 0.5*v**2)*(tt-t)) / (v * np.sqrt(tt-t))
@@ -45,3 +52,12 @@ def implied_volatility(b, x, t, c, r, v_0, tt, tol=1e-5, max_iteration=100):
         sigma -= diff / v
 
     return np.nan
+
+def RMSE (actual, prediction): 
+    diff = 0
+    for i in range(len(actual)): 
+        diff += (actual[i] - prediction[i])**2
+    
+    mean = diff/len(actual)
+    rmse = mean ** 0.5
+    return(rmse)
--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import yfinance as yf 
 import datetime
-from formulas import *
+from formulas import * 
+import pandas as pd 
 
 ticker = yf.Ticker("AAPL")
 x = ticker.history(period="1d")['Close'].iloc[-1] 
@@ -20,14 +21,32 @@ tt = T_days / 252
 K = calls['strike']
 market_prices = calls['lastPrice']
 
+## Random volatility 
 bs = [black_scholes_model(x, 0, c, 0.05, 0.05, tt) for c in K]
 
+## Historical Volatility 
 data = ticker.history(period="3mo")['Close']
 his_vol = historical_volatility(data,30).iloc[-1]
-print(his_vol)
+print("hist_vol:", his_vol)
 
 bs_hist_vol = [black_scholes_model(x, 0, c, 0.05, his_vol, tt) for c in K]
 
+## Bootstrap Historical Volatiltity 
+r = len(data)
+boot_vol_dist = bootstrap_volatility(data,r, 5000)
+
+lower = np.quantile(boot_vol_dist, 0.025)
+upper = np.quantile(boot_vol_dist, 0.975)
+
+print(lower, upper)
+
+plt.hist(boot_vol_dist)
+boot_vol = boot_vol_dist.mean()
+print('Boot vol:', boot_vol)
+
+bs_boot_vol = [black_scholes_model(x, 0, c, 0.05, boot_vol, tt) for c in K]
+
+## Implied Volatility 
 K, market_prices = zip(*[(k, mp) for k, mp in zip(K, market_prices) if mp > 0 and np.isfinite(mp)])
 implied_vols = []
 for k, mp in zip(K, market_prices):
@@ -37,10 +56,11 @@ for k, mp in zip(K, market_prices):
 
 iv_nona = [iv for iv in implied_vols if np.isfinite(iv)]
 chose_iv = np.median(iv_nona)
-print(chose_iv)
+print('Implied vol:' , chose_iv)
 
 bs_implied_vol = [black_scholes_model(x, 0, c, 0.05, chose_iv, tt) for c in K]
 
+## Random Volatility 
 plt.figure(figsize=(10,6))
 plt.plot(K, market_prices, label="Market Price", marker='o')
 plt.plot(K, bs, label="Black-Scholes Price", marker='x')
@@ -51,7 +71,7 @@ plt.ylabel("Option Price")
 plt.legend()
 plt.grid(True)
 plt.show()
-
+## Historical Volatility 
 plt.figure(figsize=(10,6))
 plt.plot(K, market_prices, label="Market Price", marker='o')
 plt.plot(K, bs_hist_vol, label="Black-Scholes Price", marker='x')
@@ -63,6 +83,19 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+## Bootstrap Volatility 
+plt.figure(figsize=(10,6))
+plt.plot(K, market_prices, label="Market Price", marker='o')
+plt.plot(K, bs_boot_vol, label="Black-Scholes Price", marker='x')
+plt.axvline(x, color='gray', linestyle='--', label='Spot Price')
+plt.title(f"AAPL Call Option Prices (Exp: {expiration}) with bootstrap historical volatility")
+plt.xlabel("Strike Price")
+plt.ylabel("Option Price")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+## Implied Volatility 
 plt.figure(figsize=(10,6))
 plt.plot(K, market_prices, label="Market Price", marker='o')
 plt.plot(K, bs_implied_vol, label="Black-Scholes Price", marker='x')
@@ -74,35 +107,21 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-errors = abs(np.array(bs) - np.array(market_prices))
-plt.figure(figsize=(10,6))
-plt.plot(K, errors, label="Model Error (BS - Market)")
-plt.axhline(0, color='gray', linestyle='--')
-plt.xlabel("Strike Price")
-plt.ylabel("Error ($)")
-plt.title("Black-Scholes Pricing Error")
-plt.grid(True)
-plt.legend()
-plt.show()
+rmse_random= RMSE(np.array(market_prices), np.array(bs))
+print('error random:' , rmse_random)
 
-error_hist =abs(np.array(bs_hist_vol) - np.array(market_prices))
-plt.figure(figsize=(10,6))
-plt.plot(K, error_hist, label="Model Error (BS - Market) historical volatility")
-plt.axhline(0, color='gray', linestyle='--')
-plt.xlabel("Strike Price")
-plt.ylabel("Error ($)")
-plt.title("Black-Scholes Pricing Error")
-plt.grid(True)
-plt.legend()
-plt.show()
+rmse_hist = RMSE(np.array(market_prices), np.array(bs_hist_vol))
+print('error hist:', rmse_hist)
 
-error_iv = abs(np.array(bs_implied_vol) - np.array(market_prices))
-plt.figure(figsize=(10,6))
-plt.plot(K, error_iv, label="Model Error (BS - Market) implied volatility")
-plt.axhline(0, color='gray', linestyle='--')
-plt.xlabel("Strike Price")
-plt.ylabel("Error ($)")
-plt.title("Black-Scholes Pricing Error implied volatility")
-plt.grid(True)
-plt.legend()
-plt.show()
+rmse_iv = RMSE(np.array(market_prices), np.array(bs_implied_vol))
+print('error implied:' , rmse_iv)
+
+rmse_boot = RMSE(np.array(market_prices), np.array(bs_boot_vol))
+print('error boot:', rmse_boot)
+
+market_prices = np.array(market_prices)
+
+relative_rmse = rmse_random/np.mean(market_prices)
+print(relative_rmse)
+
+rmse_random - rmse_hist
